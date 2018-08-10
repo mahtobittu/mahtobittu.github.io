c4413669c0d4acd72a2377e7beddaf91e61c81dc
mahtobittu <mahtobittu@gmail.com>
2018-08-10 12:07:51 +0530

    Add files via upload
    portfolio

---
+++ jssolution.js
@@ -0,0 +1,34 @@
+
+//$(document).ready(function () {
+    
+
+  //  $("div").css("display" , "flex");
+    
+    
+
+//});
+   
+    
+
+
+
+    
+    
+         
+    
+// $("button").on("click" ,function(){
+    
+//            c = $(this).text();
+//            //console.log(c);
+//            var x = c;
+//            var rnd = Math.floor(Math.random()*50);
+//            for (var i = 1; i <= rnd; i++) {
+       
+//                cat = $("#rock").append("<" + x + ">" + x + "</" + x + ">");
+       
+//            }
+
+        
+//     });
+  
+
